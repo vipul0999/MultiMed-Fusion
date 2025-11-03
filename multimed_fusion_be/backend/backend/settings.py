7696614de7abbce9b7be6db29384b5f58713eaae
Anil Panday <34656068+b-rabbit091@users.noreply.github.com>
2025-11-03 09:06:12 -0600

    Enable CORS support in Django settings for dev environment
    Added CORS headers middleware and allowed all origins.

--- multimed_fusion_be/backend/backend/settings.py
+++ multimed_fusion_be/backend/backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+        'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
